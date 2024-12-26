032571dc5f6750f1a423e47fe057ff5d6827764f
zhouqiao <zhouqiao058@hellobike.com>
2024-12-26 17:53:04 +0800

    feat: dashboard add chart

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,12 @@
-export default function Page() {
-  return <p>Dashboard Page</p>
+import  RevenueChart from "@/app/ui/dashboard/revenue-chart";
+import { fetchRevenue } from "@/app/lib/data";
+
+export default async function Page() {
+  const revenue = await fetchRevenue();
+  return (
+    <main>
+      <h1 className="text-2xl font-bold">Dashboard</h1>
+      <RevenueChart revenue={revenue} />
+    </main>
+  )
 }
