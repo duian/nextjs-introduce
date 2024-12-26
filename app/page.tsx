96ef57a59d86c6f1e06742ed01c7543c042ec6f1
zhouqiao <zhouqiao058@hellobike.com>
2024-12-26 17:22:11 +0800

    feat: add db

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
+'use client'
+
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
+// import { supabase } from '@/utils/supabase/server';
 
+// const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!);
+
+// console.log('ssssssupabase', process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
